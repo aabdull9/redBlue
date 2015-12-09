b50086be984d0090da64d9f2f9f62eca8e04dc4c
USER <user@admin-macbook-pro.local>
2015-12-09 17:03:38 +0800

    redBlue app exercise

--- redBlue/ViewController.swift
+++ redBlue/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var blueBg: UIImageView!
+    @IBOutlet weak var redBg: UIImageView!
+    @IBOutlet weak var showBlueButton: UIButton!
+    @IBOutlet weak var hideBlueButton: UIButton!
+    @IBOutlet weak var showRedButton: UIButton!
+    @IBOutlet weak var hideRedButton: UIButton!
+    @IBOutlet weak var hideBothButton: UIButton!
+    @IBOutlet weak var showBothButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func funcShowBlueImage(sender: AnyObject) {
+        blueBg.hidden = false
+    }
+
+    @IBAction func funcHideBlueImage(sender: AnyObject) {
+        blueBg.hidden = true
+
+    }
+    
+    @IBAction func funcShowRedImage(sender: AnyObject) {
+        redBg.hidden = false
+    }
+
+    @IBAction func funcHideRedImage(sender: AnyObject) {
+        
+        redBg.hidden = true
+
+    }
+    
+
+    @IBAction func funcHideBothImages(sender: AnyObject) {
+        redBg.hidden = true
+        blueBg.hidden = true
+
+    }
+    
+
+    @IBAction func funcShowBothImages(sender: AnyObject) {
+        
+        redBg.hidden = false
+        blueBg.hidden = false
+        
+    }
 
 }
-
